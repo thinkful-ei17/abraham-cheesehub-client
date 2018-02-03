8f26cdc57f7d10213a06f3c9f83dc7437d249bd0
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-02-02 22:15:03 -0800

    Update index.js to use redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CheeseList from './components/cheese-list';
 import registerServiceWorker from './registerServiceWorker';
- 
+import {Provider} from 'react-redux';
+import store from './store';
 
 const cheeses = ['Bath Blue', 'Barkham Blue', 'Buxton Blue'];
 
 ReactDOM.render(
-  <CheeseList cheeses={cheeses}/>, document.getElementById('root'));
+  <Provider store={store}>
+    <CheeseList cheeses={cheeses}/>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
