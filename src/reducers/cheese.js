832560996674d756c9b0261314a78c31b0eebb51
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-02-02 22:01:58 -0800

    Add cheese reducer

---
+++ src/reducers/cheese.js
@@ -0,0 +1,34 @@
+import {FETCH_CHEESES_REQUEST, FETCH_CHEESES_SUCCESS, FETCH_CHEESES_ERROR} from '../actions/cheese';
+
+
+const initialState = {
+  cheeses: [],
+  loading: false,
+  error: null
+};
+
+export default (state=initialState, action) => {
+  switch(action.type){
+  case FETCH_CHEESES_REQUEST:
+    return {
+      ...state,
+      loading: true,
+      error: null
+    };
+  case FETCH_CHEESES_SUCCESS:
+    return {
+      ...state,
+      cheeses: action.cheeses,
+      loading: false,
+      error: null
+    };
+  case FETCH_CHEESES_ERROR:
+    return {
+      ...state,
+      loading: false,
+      error: action.error
+    };
+  default:
+    return {state};
+  }
+};
